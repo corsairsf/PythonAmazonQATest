3cb6280eef9c1932cf6087a5b7a7f89c31d41ec5
Konstantin <kosta.nikolaev@gmail.com>
2018-04-13 11:07:27 -0700

    Add test file in pageObject

--- pageObjects/base.py
+++ pageObjects/base.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 class Page(object):
-	def __inii__(self, driver, base_url='https://amazon.com'):
+	def __init__(self, driver, base_url='https://amazon.com'):
 		self.base_url = base_url
 		self.driver = driver
 		self.timeout = 30
---
+++ pageObjects/test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from base import Page
+from locators import *
+import users
+from selenium.webdriver.support.ui import WebDriverWait
+
